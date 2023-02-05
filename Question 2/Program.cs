a948865b4c847918342b4395cc938057defc2964
abhi patel <200557634@student.georgianc.on.ca>
2023-02-05 15:18:32 -0500

    Added solution for Question 2 in C#

---
+++ Question 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Q2 a)
+            string str1 = "Supercalifragilisticexpialidocious";
+            Console.WriteLine(str1.Length);
+
+            // Q2 b)
+            string str2 = "ice";
+            Console.WriteLine(str1.Contains(str2));
+
+            // Q2 c)
+            string s1 = "Supercalifragilisticexpialidocious";
+            string s2 = "Honorificabilitudinitatibus";
+            string s3 = "Bababadalgharaghtakamminarronnkonn";
+
+            Console.WriteLine("The longest string is: ");
+            if (s1.Length >= s2.Length && s2.Length >= s3.Length)
+            {
+                Console.WriteLine(s1);
+            }
+            else if (s2.Length >= s1.Length && s2.Length >= s3.Length)
+            {
+                Console.WriteLine(s2);
+            }
+            else
+            {
+                Console.WriteLine(s3);
+            }
+
+            // Q2 d)
+             string[] words = {"Berlioz", "Borodin", "Brian", 
+            "Bartok", "Bellini", "Buxtehude", "Bernstein"};
+
+            Array.Sort(words);
+
+            Console.WriteLine(words[0]);
+        }
+    }
+}
---
+++ question 5/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace RECTANGLE
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter x coordinate:");
+            double x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter y coordinate:");
+            double y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter x1 coordinate:");
+            double x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter y1 coordinate:");
+            double y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter x2 coordinate:");
+            double x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter y2 coordinate:");
+            double y2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Inside(x, y, x1, y1, x2, y2));
+
+            // (b) checking if point (1, 1) lies in one of the given rectanlges
+
+            Console.WriteLine("(b)checking if point(1, 1) lies in one of the given rectanlges");
+
+            /*
+             * (0.3, 0.5) (1.1, 0.7) and (0.5, 0.2) (1.1, 2).
+            */
+            x = 1;
+            y = 1;
+
+            x1 = 0.3;
+            y1 = 0.5;
+            x2 = 1.1;
+            y2 = 0.7;
+
+            Console.WriteLine("(b)checking if point(1, 1) lies in rectangle 1: ");
+            Console.WriteLine(Inside(x, y, x1, y1, x2, y2));
+
+            x1 = 0.5;
+            y1 = 0.2;
+            x2 = 1.1;
+            y2 = 2;
+
+            Console.WriteLine("(b)checking if point(1, 1) lies in rectangle 2: ");
+            Console.WriteLine(Inside(x, y, x1, y1, x2, y2));
+        }
+
+        public static bool Inside(double x, double y, double x1, double y1, double x2, double y2)
+        {
+            if (x >= x1 && x <= x2 && y >= y1 && y <= y2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
