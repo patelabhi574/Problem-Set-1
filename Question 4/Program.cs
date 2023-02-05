c384debd8b2f9ea1a2f5c7af26cc84bf31a3bfe7
abhi patel <200557634@student.georgianc.on.ca>
2023-02-05 13:38:00 -0500

    Added solution for Question 4 in C#

---
+++ Question 4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Program
+
+{
+
+    public static void Main()
+
+    {
+        Console.WriteLine("Input the number of elements to be stored in the array :");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        int i;
+
+        List<int> oddNums = new List<int>(0) {};
+        List<int> evenNums = new List<int>(0) { };
+
+
+        for (i=0; i<num; i++)
+        {
+            Console.WriteLine("element - " + i + ":");
+            int addNum = Convert.ToInt32(Console.ReadLine());
+
+            if(addNum%2 == 0) 
+            {
+                evenNums.Add(addNum);
+            } else
+            {
+                oddNums.Add(addNum);
+            }
+        }
+
+        Console.WriteLine("The even numbers are:");
+        for(i=0; i<evenNums.Count; i++)
+        {
+            Console.WriteLine(evenNums[i]);
+        }
+
+        Console.WriteLine("The odd numbers are:");
+        for (i = 0; i < oddNums.Count; i++)
+        {
+            Console.WriteLine(oddNums[i]);
+        }
+    }
+
+}
